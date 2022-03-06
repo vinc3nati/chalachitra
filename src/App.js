3cb4d315520986a3982074355a8fcb6457480d92
Vinit Kanse <vinitkansend@gmail.com>
2022-03-06 17:31:29 +0530

    FEAT: fetch and displayed data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const API =
+  "https://api.themoviedb.org/4/discover/movie?api_key=14d6be44c2b7de5a4a5acb8da7c1fae6";
+
+const IMG_PREFIX = "http://image.tmdb.org/t/p/w500";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(API);
+        if (response) {
+          setData(response.data.results);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="movie-container">
+      {data &&
+        data.map((movie) => (
+          <div className="card vertical" key={movie.id}>
+            <div className="card-image-container">
+              <img
+                className="card-image"
+                src={`${IMG_PREFIX}${movie.poster_path}`}
+                alt=""
+              />
+            </div>
+            <div className="main-content">
+              <div className="card-title">
+                {movie.title}
+                <i className="fas fa-heart wishlist"></i>
+              </div>
+              <div className="card-subtitle">
+                <p>Pouplarity: {movie.popularity}</p>
+                Lang: {movie.original_language}
+              </div>
+              <div className="card-content">{movie.overview}</div>
+              <p className="rating">{movie.vote_average}</p>
+              <div className="btn-group">
+                <button className="btn primary">Add to watchlist</button>
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
